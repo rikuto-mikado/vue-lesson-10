38d717bc5c193d37211bc5be20e2ba5f349769b8
Rikuto <mikadorikuto@gmail.com>
2025-10-18 12:25:09 +0900

    add some code

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const app = Vue.createApp({
   data() {
     return { 
+      enteredGoalValue: '',
       goals: []
 
     };
   },
+  methods: {
+    // Method to add a new goal to the goals array
+    // Uses 'this' to access component's data properties (enteredGoalValue and goals)
+    // push() adds the entered value to the end of the goals array
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    }
+  }
 });
 
 app.mount('#user-goals');
+
+if (ture) {
+
+}
